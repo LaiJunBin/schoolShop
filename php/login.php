2c8d605c33347e2474ae4f60b548815b618f8eea
xyz607xx <xyz607xx@gmail.com>
2018-03-01 23:52:46 +0800

    修正session的user Key

--- php/login.php
+++ php/login.php
@@ -8,7 +8,7 @@
     $login = $select->fetch(PDO::FETCH_ASSOC);
     if($login){
         $_SESSION['login']=$login['u_username'];
-        $_SESSION['loginUser'] = $login['a_name'];
+        $_SESSION['loginUser'] = $login['u_name'];
         header('location:./order.php');
     }else{
         echo "<script>alert('帳號或密碼錯誤');history.go(-1)</script>";
